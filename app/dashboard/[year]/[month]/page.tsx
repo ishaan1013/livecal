0eb09bb993014f1c948c5e95d7e0d8e1aa27c818
ishaan1013 <69771365+ishaan1013@users.noreply.github.com>
2023-06-10 22:58:23 -0400

    start data fetching

--- app/dashboard/[year]/[month]/page.tsx
+++ app/dashboard/[year]/[month]/page.tsx
@@ -1,15 +1,52 @@
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
+
 import Calendar from "@/components/calendar/calendar";
 import DateSelect from "@/components/calendar/dateSelect";
-import { redirect } from "next/navigation";
+
+import prisma from "@/prisma";
 
 const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-export default function Dashboard({
+export default async function Dashboard({
   params,
 }: {
   params: { month: string; year: string };
 }) {
   const { month, year } = params;
+
+  validateRoute(params);
+  const view = await setupView(params);
+
+  return (
+    <div className="w-screen p-8 bg-muted flex-grow flex flex-col">
+      <div className="flex w-full justify-between items-center">
+        <DateSelect month={parseInt(month)} year={parseInt(year)} />
+        <div className="bg-background w-9 rounded-full h-9" />
+      </div>
+      <div>{JSON.stringify(view)} prisma</div>
+      {/* <pre className="whitespace-pre">{JSON.stringify(user, null, 2)}</pre> */}
+
+      {/* <div className="mt-4 w-full flex-grow relative overflow-hidden flex flex-col min-h-[600px] gap-[1px] bg-accent p-[1px] rounded-xl">
+        <div className="w-full gap-[1px] grid grid-cols-7 mb-[1px]">
+          {weekDays.map((day) => (
+            <div
+              key={day}
+              className="py-3 font-medium bg-background text-neutral-600 w-full text-center"
+            >
+              {day}
+            </div>
+          ))}
+        </div>
+        <Calendar month={parseInt(month)} year={parseInt(year)} />
+      </div> */}
+    </div>
+  );
+}
+
+const validateRoute = (params: { month: string; year: string }) => {
+  const { month, year } = params;
+
   const d = new Date();
   const currentYear = d.getFullYear();
   const currentMonth = (d.getMonth() + 1).toString().padStart(2, "0");
@@ -23,27 +60,43 @@ export default function Dashboard({
   if (isNaN(parseInt(month)) || parseInt(month) < 1 || parseInt(month) > 12) {
     redirect(`/dashboard/${currentYear}/${currentMonth}`);
   }
+};
 
-  return (
-    <div className="w-screen p-8 bg-muted flex-grow flex flex-col">
-      <div className="flex w-full justify-between items-center">
-        <DateSelect month={parseInt(month)} year={parseInt(year)} />
-        <div className="bg-background w-9 rounded-full h-9" />
-      </div>
-      {/* <div>{JSON.stringify(params)} pathname</div> */}
-      <div className="mt-4 w-full flex-grow relative overflow-hidden flex flex-col min-h-[600px] gap-[1px] bg-accent p-[1px] rounded-xl">
-        <div className="w-full gap-[1px] grid grid-cols-7 mb-[1px]">
-          {weekDays.map((day, i) => (
-            <div
-              key={i}
-              className="py-3 font-medium bg-background text-neutral-600 w-full text-center"
-            >
-              {day}
-            </div>
-          ))}
-        </div>
-        <Calendar month={parseInt(month)} year={parseInt(year)} />
-      </div>
-    </div>
-  );
-}
+const setupView = async (params: { month: string; year: string }) => {
+  const { month, year } = params;
+
+  const user = auth();
+
+  if (user.userId) {
+    const monthView = await prisma.monthView.findUnique({
+      where: {
+        MonthViewId: {
+          monthYear: `${year}-${month}`,
+          user: user.userId,
+        },
+      },
+      include: {
+        dates: {
+          include: {
+            tasks: true,
+          },
+        },
+      },
+    });
+
+    if (monthView) {
+      console.log("monthView", monthView);
+      return monthView;
+    } else {
+      const newMonthView = await prisma.monthView.create({
+        data: {
+          monthYear: `${year}-${month}`,
+          user: user.userId,
+        },
+      });
+      console.log("newMonthView", newMonthView);
+      return newMonthView;
+      // return null;
+    }
+  }
+};
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,54 +1,54 @@
-"use server";
+// "use server";
 
-import { revalidatePath } from "next/cache";
-import prisma from "../prisma";
+// import { revalidatePath } from "next/cache";
+// import prisma from "../prisma";
 
-export async function setupView(month: string, year: string) {
-  const monthView = prisma.monthView.findUnique({
-    where: {
-      monthYear: `${month}-${year}`,
-    },
-    include: {
-      dates: {
-        include: {
-          tasks: true,
-        },
-      },
-    },
-  });
+// export async function setupView(month: string, year: string) {
+//   const monthView = prisma.monthView.findUnique({
+//     where: {
+//       monthYear: `${month}-${year}`,
+//     },
+//     include: {
+//       dates: {
+//         include: {
+//           tasks: true,
+//         },
+//       },
+//     },
+//   });
 
-  // if (monthView) {
-  //   return monthView;
-  // } else {
-  //   const newMonthView = await prisma.monthView.create({
-  //     data: {
-  //       monthYear: `${month}-${year}`,
-  //       user: "test",
-  //     },
-  //   });
-  //   return newMonthView;
-  // }
-}
+//   if (monthView) {
+//     return monthView;
+//   } else {
+//     const newMonthView = await prisma.monthView.create({
+//       data: {
+//         monthYear: `${month}-${year}`,
+//         user: "test",
+//       },
+//     });
+//     return newMonthView;
+//   }
+// }
 
-export async function setupDay(month: string, year: string, day: string) {
-  // for after clicking a day on the calendar
-  // if day exists from setupView, use that data
-  // else:
-  // CREATE day view, return empty
-}
+// export async function setupDay(month: string, year: string, day: string) {
+//   // for after clicking a day on the calendar
+//   // if day exists from setupView, use that data
+//   // else:
+//   // CREATE day view, return empty
+// }
 
-export async function createTask(text: string) {
-  // CREATE task, return status
-}
+// export async function createTask(text: string) {
+//   // CREATE task, return status
+// }
 
-export async function updateTask(id: string, text: string) {
-  // UPDATE task, return status
-}
+// export async function updateTask(id: string, text: string) {
+//   // UPDATE task, return status
+// }
 
-export async function checkTask(id: string, checked: boolean) {
-  // UPDATE task, return status
-}
+// export async function checkTask(id: string, checked: boolean) {
+//   // UPDATE task, return status
+// }
 
-export async function deleteTask(id: string) {
-  // DELETE task, return status
-}
+// export async function deleteTask(id: string) {
+//   // DELETE task, return status
+// }
