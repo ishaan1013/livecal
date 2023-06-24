071067439f4388292af662645ec974d56f16f09b
ishaan1013 <69771365+ishaan1013@users.noreply.github.com>
2023-06-24 01:20:27 -0400

    add liveblocks

---
+++ lib/state/index.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@liveblocks/client";
+
+const client = createClient({
+  publicApiKey:
+    "pk_dev_HcFppTumviawSWdK9hmZ23V4gb8X3vhUN2TRaIvTcD6uhHMBeJakGa4Ay5iTkhJm",
+});
